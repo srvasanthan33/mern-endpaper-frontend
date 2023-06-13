6672dd746bd6ffd2f356abf8e8640b8510b62745
Vasanthan <srvasanthan33@outlook.com>
2023-06-13 09:39:19 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes,Route,Link } from 'react-router-dom';
+
+import HomeComponent from './components/HomeComponent/HomeComponent';
+import CartComponent from './components/CartComponent/CartComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <div className="App">
+      <header className='header'>
+        <Link to = "/" className='linker'><h2>Soulster</h2></Link>
+
+        <ul className='navbar'>
+          <li className='nav-item'>
+            <Link to ="/" className='linker'>Home</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to ="/cart" className='linker'>Cart</Link>
+          </li>
+          
+        </ul>
       </header>
+      <div className='section'>
+        <Routes>
+          <Route exact path='/' element ={<HomeComponent/>} />
+          <Route exact path ='/' element ={<CartComponent/>}/>
+        </Routes>
+      </div>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/CartComponent/CartComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class CartComponent extends Component {
+  render() {
+    return (
+      <div>CartComponent</div>
+    )
+  }
+}
+
+export default CartComponent
